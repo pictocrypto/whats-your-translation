35f13061479d5853af790b704acf6dfbe451ac3e
9sallaround <the.healing.trust.1@gmail.com>
2023-09-04 20:31:46 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -127,18 +127,29 @@ const Home: NextPage = () => {
       
 
       
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://demo.whatsyourtranslation.com/" />
-      <meta property="og:title" content="What's Your Translation?" />
-      <meta property="og:description" content="Private Web3 Sites, Gated Token Offerings & More" />
-      <meta property="og:image" content="https://ipfs.io/ipfs/QmcPuxR7hxv9jx7Hxi7FWPJLgABuDckWcdchStSjcF2BbE?filename=heyyoumini.png" />
+      <meta property="og:type" content="website"/>
+      <meta property="og:url" content="https://demo.whatsyourtranslation.com/"/>
+      <meta property="og:title" content="What's Your Translation?"/>
+      <meta property="og:description" content="Private Web3 Sites, Gated Token Offerings & More"/>
+      <meta property="og:image" content="https://ipfs.io/ipfs/QmcPuxR7hxv9jx7Hxi7FWPJLgABuDckWcdchStSjcF2BbE?filename=heyyoumini.png"/>
+      <meta property="og:image:width" content="290"/>
+      <meta property="og:image:height" content="290"/>
 
       
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://demo.whatsyourtranslation.com/" />
-      <meta property="twitter:title" content="What's Your Translation?" />
-      <meta property="twitter:description" content="Private Web3 Sites, Gated Token Offerings & More" />
-      <meta property="twitter:image" content="https://ipfs.io/ipfs/QmbHgHmBYTPc6Vtdg6EP29NNsqx7aNT5wPopbEQ6qbHajF?filename=heyyou.png" />
+      <meta property="twitter:card" content="summary_large_image"/>
+      <meta property="twitter:url" content="https://demo.whatsyourtranslation.com/"/>
+      <meta property="twitter:title" content="What's Your Translation?"/>
+      <meta property="twitter:description" content="Private Web3 Sites, Gated Token Offerings & More"/>
+      <meta property="twitter:image" content="https://ipfs.io/ipfs/QmbHgHmBYTPc6Vtdg6EP29NNsqx7aNT5wPopbEQ6qbHajF?filename=heyyou.png"/>
+        
+
+
+   
+      
+      
+      
+        
+       
         
       
       </div>
